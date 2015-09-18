08079e9a5dc90094e6a3e8bc83e15966d0333918
cajeeper <jbennett@m-oit-7vqgw12-w.msjcad.edu>
2015-09-18 09:06:31 -0700

    Revert "Main"
    This reverts commit 5280318c55419e5a39b003c5ceb17579ebb77b19.

--- Download_VSAN_HCL.ps1
+++
@@ -1,5 +0,0 @@
-$storageDir = "c:\users\username\desktop"
-$webclient = New-Object System.Net.WebClient
-$url = "http://partnerweb.vmware.com/service/vsan/all.json"
-$file = "$storageDir\all.json"
-$webclient.DownloadFile($url,$file)
