542b01df1ff5b1d2eeb08a06a915c8def00ae25f
cajeeper <jbennett@m-oit-7vqgw12-w.msjcad.edu>
2015-10-16 09:24:13 -0700

    Master
    Added ejecting virtual cd-roms, enter host maintenance, exit host
    maintenance, and move the VMs back

--- vMotion Off All VMs Host A to B.ps1
+++ vMotion Off All VMs Host A to B.ps1
@@ -1,5 +1,29 @@
+#maintenance host
 $srchostesx = "esxA.domain.local"
+#temp VM host
 $dsthostesx = "esxB.domain.local"
+
+#VMs to be migrated around
 $vms = get-vm | ? { $_.vmhost -like $srchostesx }
 
+#Disconenct any cds as needed
+# $vms | % { get-vm -name $_.name | Get-CDDrive  } | ? { $_.IsoPath -like "*.iso" -OR $_.HostDevice -match "/" } | % { $_ | Set-CDDrive -NoMedia -Confirm:$false }
+
+#move VMs off
 foreach($vm in $vms) { Move-VM $vm -Destination $dsthostesx -VMotionPriority High; sleep 10; }
+
+#enter host maintenance 
+Get-VMHost -Name $srchostesx | Set-VMHost -State Maintenance
+#enter host maintenance - VSAN
+# Get-View -ViewType HostSystem -Filter @{"Name" = $srchostesx }|?{!$_.Runtime.InMaintenanceMode}|%{$_.EnterMaintenanceMode(0, $false, (new-object VMware.Vim.HostMaintenanceSpec -Property @{vsanMode=(new-object VMware.Vim.VsanHostDecommissionMode -Property @{objectAction=[VMware.Vim.VsanHostDecommissionModeObjectAction]::NoAction})}))}
+
+
+#
+# do my thang
+#
+
+#exit host maintenance
+Get-VMHost -name $srchostesx | Set-VMHost -State Connected
+
+#move VMs back
+foreach($vm in $vms) { Move-VM $vm -Destination $srchostesx -VMotionPriority High; sleep 10; }
