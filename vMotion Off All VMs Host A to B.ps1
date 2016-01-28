ef9caf03697ca800b0f4c22a87d57e17c0ada0f3
cajeeper <jbennett@m-oit-7vqgw12-w.msjcad.edu>
2016-01-28 11:39:08 -0800

    Master

--- vMotion Off All VMs Host A to B.ps1
+++ vMotion Off All VMs Host A to B.ps1
@@ -25,17 +25,17 @@ $srchostesx = "esxA.domain.local"
 $dsthostesx = "esxB.domain.local"
 
 #VMs to be migrated around
-$vms = get-vm | ? { $_.vmhost -like $srchostesx }
+$VMs = get-vm | ? { $_.vmhost -like $srchostesx }
 
-#Disconenct any cds as needed
-# $vms | % { get-vm -name $_.name | Get-CDDrive  } | ? { $_.IsoPath -like "*.iso" -OR $_.HostDevice -match "/" } | % { $_ | Set-CDDrive -NoMedia -Confirm:$false }
+#disconenct any ISOs as needed
+# $VMs | % { get-vm -name $_.name | Get-CDDrive  } | ? { $_.IsoPath -like "*.iso" -OR $_.HostDevice -match "/" } | % { $_ | Set-CDDrive -NoMedia -Confirm:$false }
 
 #move VMs off
-if ($vms.Count -gt 0) { $vms | % {$i=0} { 
+if ($VMs.Count -gt 0) { $VMs | % {$i=0} { 
 	$i++
-	if($showProgress) { Write-Progress -Activity "vMotion Off All VMs: $($srchostesx) to $($dsthostesx)" -Status "$($i)/$($vms.Count): VM:$($_.Name) - Attempting to vMotion to $($dsthostesx)..." -PercentComplete (($i/$vms.Count)*100) }
+	if($showProgress) { Write-Progress -Activity "vMotion Off All VMs: $($srchostesx) to $($dsthostesx)" -Status "$($i)/$($VMs.Count): VM:$($_.Name) - Attempting to vMotion to $($dsthostesx)..." -PercentComplete (($i/$VMs.Count)*100) }
 	Move-VM $_ -Destination $dsthostesx -VMotionPriority High
-	if($showProgress) { Write-Progress -Activity "vMotion Off All VMs: $($srchostesx) to $($dsthostesx)" -Status "$($i)/$($vms.Count): VM:$($_.Name) - Pausing 10 seconds..." -PercentComplete (($i/$vms.Count)*100) }
+	if($showProgress) { Write-Progress -Activity "vMotion Off All VMs: $($srchostesx) to $($dsthostesx)" -Status "$($i)/$($VMs.Count): VM:$($_.Name) - Pausing 10 seconds..." -PercentComplete (($i/$VMs.Count)*100) }
 	sleep 10
 	}
 }
@@ -53,11 +53,11 @@ Get-VMHost -Name $srchostesx | Set-VMHost -State Maintenance
 Get-VMHost -name $srchostesx | Set-VMHost -State Connected
 
 #move VMs back
-if ($vms.Count -gt 0) { $vms | % {$i=0} { 
+if ($VMs.Count -gt 0) { $VMs | % {$i=0} { 
 	$i++
-	if($showProgress) { Write-Progress -Activity "vMotion Off All VMs: $($dsthostesx) to $($srchostesx)" -Status "$($i)/$($vms.Count): VM:$($_.Name) - Attempting to vMotion to $($srchostesx)..." -PercentComplete (($i/$vms.Count)*100) }
+	if($showProgress) { Write-Progress -Activity "vMotion Off All VMs: $($dsthostesx) to $($srchostesx)" -Status "$($i)/$($VMs.Count): VM:$($_.Name) - Attempting to vMotion to $($srchostesx)..." -PercentComplete (($i/$VMs.Count)*100) }
 	Move-VM $_ -Destination $srchostesx -VMotionPriority High
-	if($showProgress) { Write-Progress -Activity "vMotion Off All VMs: $($dsthostesx) to $($srchostesx)" -Status "$($i)/$($vms.Count): VM:$($_.Name) - Pausing 10 seconds..." -PercentComplete (($i/$vms.Count)*100) }
+	if($showProgress) { Write-Progress -Activity "vMotion Off All VMs: $($dsthostesx) to $($srchostesx)" -Status "$($i)/$($VMs.Count): VM:$($_.Name) - Pausing 10 seconds..." -PercentComplete (($i/$VMs.Count)*100) }
 	sleep 10
 	}
 }
